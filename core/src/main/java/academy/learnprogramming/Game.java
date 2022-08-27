2063f9d3521294fc2077f36aea1ddf4540835fc4
Kappa97 <ionlefter21@gmail.com>
2022-08-27 17:54:24 +0300

    -create simple game

---
+++ core/src/main/java/academy/learnprogramming/Game.java
@@ -0,0 +1,26 @@
+package academy.learnprogramming;
+
+public interface Game {
+
+    int getNumber();
+
+    int getGuess();
+
+    void setGuess(int guess);
+
+    int getSmallest();
+
+    int getBiggest();
+
+    int setRemainingGuesses();
+
+    void reset();
+
+    void check();
+
+    boolean isValidNumberRange();
+
+    boolean isGameWon();
+
+    boolean isGameLost();
+}
--- src/main/java/academy/learnprogramming/Main.java
+++
@@ -1,7 +0,0 @@
-package academy.learnprogramming;
-
-public class Main {
-    public static void main(String[] args) {
-        System.out.println("Hello");
-    }
-}
